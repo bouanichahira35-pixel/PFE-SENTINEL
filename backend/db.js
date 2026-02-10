c4b1d3625bf735e93174f712f1acd693d0eaf3de
Chahira Bouani <bouanichahira35@gmail.com>
2026-02-10 22:25:22 +0100

    add backend models and routes

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+
 const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/pfe_sentinel';
+
 mongoose.connect(uri)
   .then(() => console.log('Mongo connecté'))
-  .catch(err => console.error('Mongo erreur:', err));
+  .catch((err) => console.error('Mongo erreur:', err));
+
 module.exports = mongoose;
